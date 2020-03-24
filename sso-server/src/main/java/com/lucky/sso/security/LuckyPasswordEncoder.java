7811730d4de1eb88081758d6cd358d94b9bfe540
seyi.zhou <seyi.zhou@luckincoffee.com>
2020-03-24 20:57:04 +0800

    添加邮箱重置密码

---
+++ sso-server/src/main/java/com/lucky/sso/security/LuckyPasswordEncoder.java
@@ -0,0 +1,25 @@
+package com.lucky.sso.security;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+//import cas-org.apereo.cas.configuration.CasConfigurationProperties;
+
+//import org.apereo.cas.configuration.model.support.pm.PasswordManagementProperties;
+//import org.apereo.cas.services.RegisteredServicesEventListener;
+
+public class LuckyPasswordEncoder implements PasswordEncoder {
+
+	@Override
+	public String encode(CharSequence rawPassword) {
+		System.out.println("rawpass:" + rawPassword);
+		return rawPassword.toString();
+	}
+
+	@Override
+	public boolean matches(CharSequence rawPassword, String encodedPassword) {
+		System.out.println("rawpass2:" + rawPassword);
+		System.out.println("encodedPassword:" + encodedPassword);
+		// TODO Auto-generated method stub
+		return encodedPassword.equals(rawPassword);
+	}
+
+}
