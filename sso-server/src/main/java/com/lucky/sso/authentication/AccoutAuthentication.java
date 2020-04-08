0962f04b58a7f18a1db82f8a1feb101de979ecd8
seyi.zhou <seyi.zhou@luckincoffee.com>
2020-04-08 11:29:41 +0800

    支持员工号/邮箱/手机号登录

--- sso-server/src/main/java/com/lucky/sso/authentication/AccoutAuthentication.java
+++ sso-server/src/main/java/com/lucky/sso/authentication/AccoutAuthentication.java
@@ -2,45 +2,83 @@ package com.lucky.sso.authentication;
 
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
 
+import javax.security.auth.login.AccountNotFoundException;
+import javax.security.auth.login.FailedLoginException;
+import javax.sql.DataSource;
+
+import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import org.apereo.cas.adaptors.jdbc.QueryAndEncodeDatabaseAuthenticationHandler;
 import org.apereo.cas.authentication.AuthenticationHandlerExecutionResult;
-import org.apereo.cas.authentication.MessageDescriptor;
 import org.apereo.cas.authentication.PreventedException;
 import org.apereo.cas.authentication.UsernamePasswordCredential;
-import org.apereo.cas.authentication.handler.support.AbstractUsernamePasswordAuthenticationHandler;
+import org.apereo.cas.authentication.exceptions.AccountDisabledException;
+import org.apereo.cas.authentication.exceptions.AccountPasswordMustChangeException;
 import org.apereo.cas.authentication.principal.PrincipalFactory;
 import org.apereo.cas.services.ServicesManager;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
 
 /**
  * @author Seyi.Zhou
  * @date:  2020年3月30日 上午10:47:52
  * @Description: 身份认证
  */
-public class AccoutAuthentication extends AbstractUsernamePasswordAuthenticationHandler  {
+public class AccoutAuthentication extends QueryAndEncodeDatabaseAuthenticationHandler  {
 	
-	public AccoutAuthentication(String name, ServicesManager servicesManager, PrincipalFactory principalFactory, Integer order) {
-        super(name, servicesManager, principalFactory, order);
+	public AccoutAuthentication(final String name, final ServicesManager servicesManager, final PrincipalFactory principalFactory,
+            final Integer order, final DataSource dataSource, final String algorithmName, final String sql, 
+            final String passwordFieldName, final String saltFieldName, final String expiredFieldName, final String disabledFieldName,
+            final String numberOfIterationsFieldName, final long numberOfIterations, final String staticSalt) {
+        super(name, servicesManager, principalFactory, order, dataSource, algorithmName, sql, passwordFieldName,
+        		saltFieldName, expiredFieldName, disabledFieldName, numberOfIterationsFieldName, numberOfIterations,
+        		staticSalt);
     }
 
 	@Override
 	protected AuthenticationHandlerExecutionResult authenticateUsernamePasswordInternal(
-			UsernamePasswordCredential credential, String originalPassword)
+			final UsernamePasswordCredential transformedCredential,
+            final String originalPassword)
 			throws GeneralSecurityException, PreventedException {
 
+		if (StringUtils.isBlank(this.sql) || StringUtils.isBlank(this.algorithmName) || getJdbcTemplate() == null) {
+            throw new GeneralSecurityException("Authentication handler is not configured correctly");
+        }
 
-		String username = credential.getUsername();
-		String password = credential.getPassword();
-		
-		// 用户密码验证逻辑放这里
-		
-		//可自定义返回给客户端的多个属性信息
-        HashMap<String, Object> returnInfo = new HashMap<>();
-        returnInfo.put("expired", "11111");
-		
-        final List<MessageDescriptor> warnings = new ArrayList<>();
-		return createHandlerResult(credential,
-                this.principalFactory.createPrincipal(username, returnInfo), warnings);
+        final String loginName = transformedCredential.getUsername();
+        try {
+        	String querySql = this.sql.replace("?", loginName);
+        	//Account account = getJdbcTemplate().queryForObject(querySql, Account.class);
+            final Map<String, Object> values = getJdbcTemplate().queryForMap(querySql);
+            final String digestedPassword = digestEncodedPassword(transformedCredential.getPassword(), values);
+
+            if (!values.get(this.passwordFieldName).equals(digestedPassword)) {
+                throw new FailedLoginException("Password does not match value on record.");
+            }
+            if (StringUtils.isNotBlank(this.expiredFieldName) && values.containsKey(this.expiredFieldName)) {
+                final String dbExpired = values.get(this.expiredFieldName).toString();
+                if (BooleanUtils.toBoolean(dbExpired) || "1".equals(dbExpired)) {
+                    throw new AccountPasswordMustChangeException("Password has expired");
+                }
+            }
+            if (StringUtils.isNotBlank(this.disabledFieldName) && values.containsKey(this.disabledFieldName)) {
+                final String dbDisabled = values.get(this.disabledFieldName).toString();
+                if (BooleanUtils.toBoolean(dbDisabled) || "1".equals(dbDisabled)) {
+                    throw new AccountDisabledException("Account has been disabled");
+                }
+            }
+            return createHandlerResult(transformedCredential, this.principalFactory.createPrincipal(loginName), new ArrayList<>(0));
+
+        } catch (final IncorrectResultSizeDataAccessException e) {
+            if (e.getActualSize() == 0) {
+                throw new AccountNotFoundException(loginName + " not found with SQL query");
+            }
+            throw new FailedLoginException("Multiple records found for " + loginName);
+        } catch (final DataAccessException e) {
+            throw new PreventedException("SQL exception while executing query for " + loginName, e);
+        }
 	}
 }
--- sso-server/src/main/java/com/lucky/sso/config/CountAuthenticationConfiguration.java
+++ sso-server/src/main/java/com/lucky/sso/config/CountAuthenticationConfiguration.java
@@ -3,14 +3,26 @@ package com.lucky.sso.config;
 import org.apereo.cas.authentication.AuthenticationEventExecutionPlan;
 import org.apereo.cas.authentication.AuthenticationEventExecutionPlanConfigurer;
 import org.apereo.cas.authentication.AuthenticationHandler;
-import org.apereo.cas.authentication.principal.DefaultPrincipalFactory;
+import org.apereo.cas.authentication.CoreAuthenticationUtils;
+import org.apereo.cas.authentication.principal.PrincipalFactory;
+import org.apereo.cas.authentication.principal.PrincipalNameTransformerUtils;
+import org.apereo.cas.authentication.support.password.PasswordEncoderUtils;
+import org.apereo.cas.authentication.support.password.PasswordPolicyConfiguration;
 import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.configuration.model.support.jdbc.JdbcAuthenticationProperties;
+import org.apereo.cas.configuration.model.support.jdbc.QueryEncodeJdbcAuthenticationProperties;
+import org.apereo.cas.configuration.support.JpaBeans;
 import org.apereo.cas.services.ServicesManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+import org.apache.commons.lang3.StringUtils;
 
 import com.lucky.sso.authentication.AccoutAuthentication;
 
@@ -19,28 +31,55 @@ import com.lucky.sso.authentication.AccoutAuthentication;
 public class CountAuthenticationConfiguration implements AuthenticationEventExecutionPlanConfigurer  {
 	@Autowired
 	private CasConfigurationProperties casProperties;
+	
+	@Autowired(required = false)
+   // @Qualifier("queryAndEncodePasswordPolicyConfiguration")
+    private PasswordPolicyConfiguration queryAndEncodePasswordPolicyConfiguration;
 
-	@Override
-	public void configureAuthenticationExecutionPlan(AuthenticationEventExecutionPlan plan) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	/*
+	@Autowired
+	@Qualifier("jdbcPrincipalFactory")
+    public PrincipalFactory jdbcPrincipalFactory;
+	
 	@Autowired
 	@Qualifier("servicesManager")
 	private ServicesManager servicesManager;
 
-	@SuppressWarnings("deprecation")
+	@Bean(name = "jdbcAuthenticationHandlers")
+    public Collection<AuthenticationHandler> jdbcAuthenticationHandlers() {
+        final Collection<AuthenticationHandler> handlers = new HashSet<>();
+        handlers.add(myAuthenticationHandler());
+        return handlers;
+    }
+	
 	@Bean
 	public AuthenticationHandler myAuthenticationHandler() {
-		return new AccoutAuthentication(AccoutAuthentication.class.getName(),
-				servicesManager, new DefaultPrincipalFactory(), 1);
+		final JdbcAuthenticationProperties jdbc = casProperties.getAuthn().getJdbc();
+		final QueryEncodeJdbcAuthenticationProperties b = jdbc.getEncode().get(0);
+		
+		final AccoutAuthentication h = new AccoutAuthentication(b.getName(), servicesManager,
+				jdbcPrincipalFactory, b.getOrder(), JpaBeans.newDataSource(b), b.getAlgorithmName(), b.getSql(), b.getPasswordFieldName(),
+	            b.getSaltFieldName(), b.getExpiredFieldName(), b.getDisabledFieldName(), b.getNumberOfIterationsFieldName(), b.getNumberOfIterations(),
+	            b.getStaticSalt());
+
+	        h.setPasswordEncoder(PasswordEncoderUtils.newPasswordEncoder(b.getPasswordEncoder()));
+	        h.setPrincipalNameTransformer(PrincipalNameTransformerUtils.newPrincipalNameTransformer(b.getPrincipalTransformation()));
+
+	        if (queryAndEncodePasswordPolicyConfiguration != null) {
+	            h.setPasswordPolicyConfiguration(queryAndEncodePasswordPolicyConfiguration);
+	        }
+
+	        h.setPrincipalNameTransformer(PrincipalNameTransformerUtils.newPrincipalNameTransformer(b.getPrincipalTransformation()));
+
+	        if (StringUtils.isNotBlank(b.getCredentialCriteria())) {
+	            h.setCredentialSelectionPredicate(CoreAuthenticationUtils.newCredentialSelectionPredicate(b.getCredentialCriteria()));
+	        }
+
+	        //LOGGER.debug("Created authentication handler [{}] to handle database url at [{}]", h.getName(), b.getUrl());
+	        return h;
 	}
 
 	@Override
 	public void configureAuthenticationExecutionPlan(final AuthenticationEventExecutionPlan plan) {
 		plan.registerAuthenticationHandler(myAuthenticationHandler());
 	}
-	*/
 }
